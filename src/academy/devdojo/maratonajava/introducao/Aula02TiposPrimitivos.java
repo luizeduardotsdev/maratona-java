22f0e79503b15ce70df28af84bf3a8cbd478038c
luwiz <le9851928@gmail.com>
2025-04-17 11:59:00 -0300

    Aulas adicionadas

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 20;
+        long bigNum = 100000;
+        double salaryDouble = 2000;
+        float salatyFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 15;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("A idade é "+age+" anos");
+        System.out.println(verdadeiro);
+    }
+}
