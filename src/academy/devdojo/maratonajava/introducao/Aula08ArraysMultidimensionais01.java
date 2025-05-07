70a2f331b02a0c5b894399a03be6086e6a8007db
luwiz <le9851928@gmail.com>
2025-05-07 18:09:00 -0300

    arrays

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -2,6 +2,28 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args){
-        int[][] days = new int[12][];
+        int[][] days = new int[3][3];
+        days[0][0] = 31;
+        days[0][1] = 28;
+        days[0][2] = 31;
+
+        days[1][0] = 31;
+        days[1][1] = 28;
+        days[1][2] = 31;
+
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.println(days[i][j]);
+            }
+        }
+
+        System.out.println("---------------------");
+
+        for(int[] arr: days){
+            for(int num: arr){
+                System.out.println(num);
+            }
+        }
     }
+
 }
